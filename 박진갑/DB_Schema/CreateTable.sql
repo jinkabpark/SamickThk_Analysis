8b442f2dbbf5a799c1386acfcf383ea710c18923
Silver Park <silver6h@daum.net>
2024-06-09 19:31:18 +0900

    Update CreateTable.sql
    table 내용변경

--- 박진갑/DB_Schema/CreateTable.sql
+++ 박진갑/DB_Schema/CreateTable.sql
@@ -1,3 +1,9 @@
+============================================================================ 
+file name   : CreateTable.sql
+author      : JK Park
+desc   
+   1.    2024-06-01 최초생성
+
 -- ============================================================================ 
 -- Table Space 명   : Master
 -- 내 용            : Master관련 table을 관리하는 table space
@@ -11,7 +17,7 @@
 --            3     tMstEqp                    1 K
 --            4     tMstOperation              1 K
 --            5     tMstBottle                 1 K
---            6     tMstRoute                  1 K
+--            6     tMstRouteOper              1 K
 --            7     tMstScript4Manipulator     1 K
 --            8     tMstScript4UR              1 K
 --            9     tMstScript4CoOperRobot     1 K
@@ -24,7 +30,7 @@
 --           No.    Table 명             Size   
 --          ----  --------------------  ------------
 --            1     tProcBottle
---            2     tProcBotCurrOper
+--            2     tProcBotOper
 --            3     tChgEqpStatus
 --            4     tDailyEqpStatus
 
@@ -35,8 +41,8 @@
 -- 관련 Master Table
 --           No.    Table 명             Size   
 --          ----  --------------------  ------------
---            1     tProcBotHistOper
-
+--            1     tHisProcBotOper
+--            2     tHisChgEqpStatus
 -- ========================================================================================
 --   Table No             : 1
 --   Table 명              : tMstUser
@@ -149,7 +155,7 @@ INSERT INTO tMstEqp VALUES ('1', '1', N'반출입기', "Idle", "LoadReq", null);
 INSERT INTO tMstEqp VALUES ('2', '1', N'분석기', "Idle", "LoadReq", null);
 INSERT INTO tMstEqp VALUES ('3', '1', N'Stocker', "Idle", "LoadReq", null);      -- Stocker #1호기
 INSERT INTO tMstEqp VALUES ('4', '1', N'폐기설비', "Idle", "LoadReq", null);
-INSERT INTO tMstEqp VALUES ('5', '1', N'이동형협업로봇', "Idle", "LoadReq", null);
+INSERT INTO tMstEqp VALUES ('5', '1', N'이동형협업로봇', "Idle", "LoadComp", null);
 
 
 -- ========================================================================================
@@ -186,6 +192,13 @@ ALTER TABLE tMstOperation
       ADD CONSTRAINT tMstOperation_PK PRIMARY KEY (EqpTypeID, OperID) ON [MasterIdx];
 ALTER TABLE tMstOperation 
       ADD CONSTRAINT tMstEqp_FK FOREIGN KEY (EqpGroupID) REFERENCES tMstEqpGroup(EqpGroupID) ON [MasterIdx];
+
+INSERT INTO tMstOperation VALUES ('1', '1', N'공 Bottle 초기상태(반출대기)');     -- 반출입기에서 반출을 대기중인 공 Bottle(작업자가 시료채취를 위해)
+INSERT INTO tMstOperation VALUES ('1', '2', N'시료채취중');              -- 반출입기 작업자공간쪽 Port에서 unloadcomp 수신하면 (/w Bottle Barcode)
+INSERT INTO tMstOperation VALUES ('1', '3', N'실 Bottle 투입');              -- 반출입기 작업자공간쪽 Port에서 Loadcomp하면 (/w Bottle Barcode)
+INSERT INTO tMstOperation VALUES ('1', '4', N'실 Bottle 출하대기');    -- UnloadRequest event
+INSERT INTO tMstOperation VALUES ('1', '5', N'세정후 Bottle 투입');    -- 세정후 재사용을 위하여 반출입기 분석기 Port에 Bottle 투입
+                                                                    -- 반출입기 분석실 Port에서 Loadcomp 호출할때
 
 INSERT INTO tMstOperation VALUES ('1', '1', N'Bottle 반출요청');     -- 작업자가 시료채취를 위해 반출요청
 INSERT INTO tMstOperation VALUES ('1', '2', N'Bottle 반출');              -- 시료채취
@@ -230,7 +243,7 @@ ALTER TABLE tMstBottle
 
 -- ========================================================================================
 --   Table No             : 6
---   Table 명             : tMstRoute
+--   Table 명             : tMstRouteOper
 --   내  용                : LIMS Route을 관리하는 Table
 --                         분석실에서 현재 1개 Route로 처리가능함
 --                         분석기에서 판정후 Stocker Or 폐기/모사 이동할수 있음
@@ -249,9 +262,9 @@ ALTER TABLE tMstBottle
 --          1. 2024-06-01 : 최초 생성
 --
 -- ========================================================================================
-DROP TABLE tMstRoute;
+DROP TABLE tMstRouteOper;
 
-CREATE TABLE tMstRoute (
+CREATE TABLE tMstRouteOper (
    RouteID                CHAR(1) NOT NULL,         -- Route ID
    -- 현공정에 대한 정의  
    CurrEqpGroupID         CHAR(1) NOT NULL,         -- 현재 설비군 ID 
@@ -261,17 +274,19 @@ CREATE TABLE tMstRoute (
    NextOperID             CHAR(1) NOT NULL          -- 차기 작업 ID
 ) ON [Master];
 
-ALTER TABLE tMstRoute
+ALTER TABLE tMstRouteOper
        ADD CONSTRAINT tMstRoute_PK PRIMARY KEY (RouteID, CurrEqpGroupID, CurrOperID, NextEqpGroupID) ON [MasterIdx];
 
-INSERT INTO tMstRoute VALUES ('1', '0', '0', '1', '1');
-INSERT INTO tMstRoute VALUES ('1', '1', '1', '1', '2');
-INSERT INTO tMstRoute VALUES ('1', '1', '2', '1', '3');
-INSERT INTO tMstRoute VALUES ('1', '1', '3', '3', '1');     -- UnloadRequest event (Stocker 이동)
-INSERT INTO tMstRoute VALUES ('1', '1', '3', '2', '1');     -- UnloadRequest event (분석기로 이동)
-INSERT INTO tMstRoute VALUES ('1', '3', '1', '2', '1');
-INSERT INTO tMstRoute VALUES ('1', '2', '1', '3', '1');     -- 분석후 Stocker 이동
-INSERT INTO tMstRoute VALUES ('1', '2', '1', '5', '1');     -- 분석후 폐기/모사 이동
+INSERT INTO tMstRouteOper VALUES ('1', '0', '0', '1', '1');
+INSERT INTO tMstRouteOper VALUES ('1', '1', '1', '1', '2');     -- 작업자가 시료채취를 위해 반출요청
+INSERT INTO tMstRouteOper VALUES ('1', '1', '2', '1', '3');     -- 시료채취
+INSERT INTO tMstRouteOper VALUES ('1', '1', '3', '1', '4');     -- 작업자가 시료채취후 투입
+INSERT INTO tMstRouteOper VALUES ('1', '1', '4', '3', '1');     -- UnloadRequest event (Stocker 이동)
+INSERT INTO tMstRouteOper VALUES ('1', '1', '5', '1', '1');     -- 세정후 재사용을 위하여 Bottle 반출입기에 보관
+
+INSERT INTO tMstRouteOper VALUES ('1', '2', '1', '3', '1');     -- 분석후 Stocker 이동
+INSERT INTO tMstRouteOper VALUES ('1', '3', '1', '2', '1');
+INSERT INTO tMstRouteOper VALUES ('1', '2', '1', '5', '1');     -- 분석후 폐기/모사 이동
 
 -- ========================================================================================
 --   Table No             : 7
@@ -401,7 +416,10 @@ INSERT INTO tMstScript4CoOperRobot VALUES ('1', '3', 8, "UR", '{"Body":"0, -1.57
 --   Table 명             : tProcBottle
 --   내  용                : LIMS Bottle을 관리하는 Table
 --                         tMstBottle에서는 Bottle정보가 변경되지 않는 것을 관리하고
---                         tProcBottle에서는 공정진행하면서 정보가 변경되는 것을 별도 Table(tProcBotCurrOper)에서 관리
+--                         tProcBottle에서는 공정진행하면서 정보가 변경되는 것을 별도 Table(tProcBotOper)에서 관리
+--
+--                         착공일때(LoadComp수신) CurrEqpGroupID, CurrEqpSeqNo Set한다.
+--                         완공일때(UnloadComp수신) tMstRoute table에서 Next정보를 얻고, NextEqpGroupID Set한다.
 --
 --   성  격               : Process
 --   보존기간              : 영구 (순환사용)
@@ -419,16 +437,24 @@ INSERT INTO tMstScript4CoOperRobot VALUES ('1', '3', 8, "UR", '{"Body":"0, -1.57
 DROP TABLE tProcBottle;
 
 CREATE TABLE tProcBottle (
-   BottleID               CHAR(1) NOT NULL,         -- Bottle ID
-   EqpGroupID             CHAR(1) NOT NULL,         -- 현재 설비군 ID 
+   BottleID               CHAR(15) NOT NULL,        -- Bottle ID
+   -- 현공정에 대한 정의
+   CurrEqpGroupID         CHAR(1) NOT NULL,         -- 현재 설비군 ID 
+   CurrEqpSeqNo           CHAR(1) NOT NULL,         -- 호기 (1부터 시작)
+   CurrOperID             CHAR(1) NOT NULL,         -- 현재 작업 ID
+   -- 다음공정에 대한 정의  
    NextEqpGroupID         CHAR(1) NOT NULL,         -- 차기 설비군 ID 
-   
+   NextOperID             CHAR(1) NOT NULL,         -- 현재 작업 ID
+   -- 빈병일 경우 Null (특히 반출입기)
    ExperimentRequestName  NVARCHAR(10),             -- 실험의뢰자
-   CurrLiquid             NVARCHAR(10),             -- 용액종료 (산, 염기, 유기)
+   CurrLiquid             NVARCHAR(10),             -- 용액종료 (Acid:산, Base:염기, Organic:유기)
    RequestDate            DATETIME,                 -- 요청일시
-   DispatchingPriority    TINYINT,                  -- 반송우선순위 (1:가장 낮음, 9:Hot Run)
    Position               CHAR(4),                  -- Bottle 반출입기, Stocker에서 위치정보
                                                     -- '0000'인 경우 반출 또는 이동중인 Bottle
+   StartTime              DATETIME NOT NULL,        -- 착공시간
+   EndTime                DATETIME,                 -- 완공시간
+   DispatchingPriority    TINYINT,                  -- 반송우선순위 (1:가장 낮음, 9:Hot Run)
+   EventTime              DATETIME,                 -- Event Time (장비에 입고완료시점)
    PrevLiquid             NVARCHAR(10),             -- 이전 작업에서 용액종료 (산, 염기, 유기)
 ) ON [Process];
 
@@ -439,11 +465,15 @@ ALTER TABLE tProcBottle
 
 -- ========================================================================================
 --   Table No             : 2
---   Table 명             : tProcBotCurrOper
+--   Table 명             : tProcBotOper
 --   내  용                : LIMS Bottle에서 작업이력을 관리하는 Table
 --                         tMstBottle에서는 Bottle정보가 변경되지 않는 것을 관리하고
 --                         tProcBottle에서는 공정진행하면서 정보가 변경되는 것을 별도 Table 관리
---
+
+--							재사용을 위하여 Bottle 반출입기에 입고할때(OpCode=5) tHisProcBotOper table 데이터 이동하고, 
+--                          기존 Data 삭제한다.
+--							6개월 경과한 Garbage Data 자동 삭제를 한다.
+-- 
 --   성  격               : Process
 --   보존기간              : 영구
 --   Record 발생건수(1일)   :
@@ -451,18 +481,18 @@ ALTER TABLE tProcBottle
 --   Record size          : 25
 --   Total size           : 180 = 5 * 36
 --   관리화면 유/무           : 유
---   P.K                  : BottleID
+--   P.K                  : BottleID, EqpGroupID, OperID, StartTime
 --   관련 Table            : 
 --   이 력
 --          1. 2024-06-01 : 최초 생성
 --
 -- ========================================================================================
-DROP TABLE tProcBotCurrOper;
+DROP TABLE tProcBotOper;
 
-CREATE TABLE tProcBotCurrOper (
-   BottleID               CHAR(1) NOT NULL,         -- Bottle ID
+CREATE TABLE tProcBotOper (
+   BottleID               CHAR(15) NOT NULL,        -- Bottle ID
+   -- 현공정에 대한 정의
    EqpGroupID             CHAR(1) NOT NULL,         -- 설비군 ID 
-   --OperGroupID          CHAR(1) NOT NULL,         -- Operation Group 
    OperID                 CHAR(1) NOT NULL,         -- 작업 ID  
    StartTime              DATETIME NOT NULL,        -- 착공시간
    EndTime                DATETIME,                 -- 완공시간
@@ -470,8 +500,8 @@ CREATE TABLE tProcBotCurrOper (
    Position               CHAR(4)                   -- Bottle 반출입기, Stocker에서 위치정보
 ) ON [Process];
 
-ALTER TABLE tProcBotCurrOper
-      ADD  CONSTRAINT ttProcBotCurrOper_PK PRIMARY KEY (BottleID, EqpGroupID, OperID, StartTime) ON [ProcessIdx];
+ALTER TABLE tProcBotOper
+      ADD  CONSTRAINT tProcBotOper_PK PRIMARY KEY (BottleID, EqpGroupID, OperID, StartTime) ON [ProcessIdx];
 
 -- ========================================================================================
 --   Table No             : 3
@@ -479,16 +509,18 @@ ALTER TABLE tProcBotCurrOper
 --   내  용                : 장비사용시간 관리하는 Table
 --                          Bottle 반출입기, Stocker는 설비내 Bottle 항상 있는 상태이므로 의미 없음
 --                          분석기, 폐기/모사에 대한 장비상태 관리
---                          날짜가 변경될때 관리할 방법 고민필요
 --
+--                          1일 단위로 tDailyEqpStatus summary data만들고, 
+--                          50일 경과된 데이터는 tHisChgEqpStatus table 이동하고 data 삭제한다.
+--							6개월 경과한 Garbage Data 자동 삭제를 한다.
 --   성  격               : Process
 --   보존기간              : 3년
 --   Record 발생건수(1일)   : 
---   Total Record 수      : 300개
+--   Total Record 수      : ??개
 --   Record size          : 28
 --   Total size           : 180 = 5 * 36
 --   관리화면 유/무           : 유
---   P.K                  : BottleID
+--   P.K                  : EqpGroupID, EqpSeqNo, EqpStatus, StartTime
 --   관련 Table            : 
 --   이 력
 --          1. 2024-06-01 : 최초 생성
@@ -499,13 +531,13 @@ DROP TABLE tChgEqpStatus;
 CREATE TABLE tChgEqpStatus (
    EqpGroupID             CHAR(1) NOT NULL,         -- 설비군ID
    EqpSeqNo               CHAR(1) NOT NULL,         -- 호기 (1부터 시작)
-   EqpStatus              NVARCHAR(10),             -- 장비상태(Run, Idle, Trouble, Maintenance)
-   StartTime              DATETIME,                 -- 시작시간
-   EndTime                DATETIME                  -- 종료시간
+   EqpStatus              NVARCHAR(10) NOT NULL,    -- 장비상태(Run, Idle, Trouble, Maintenance)
+   StartTime              DATETIME NOT NULL,        -- 장비상태 시작시간
+   EndTime                DATETIME                  -- 장비상태 종료시간
 ) ON [Process];
 
 ALTER TABLE tChgEqpStatus
-       ADD  CONSTRAINT tChgEqpStatus_PK PRIMARY KEY (EqpGroupID, EqpSeqNo) ON [ProcessIdx];
+       ADD  CONSTRAINT tChgEqpStatus_PK PRIMARY KEY (EqpGroupID, EqpSeqNo, EqpStatus, StartTime) ON [ProcessIdx];
 ALTER TABLE tChgEqpStatus 
       ADD CONSTRAINT tChgEqpStatus_FK FOREIGN KEY (EqpGroupID, EqpSeqNo) REFERENCES tMstEqpGroup(EqpGroupID, EqpSeqNo) ON [ProcessIdx];
 
@@ -522,7 +554,7 @@ ALTER TABLE tChgEqpStatus
 --   Record size          : 20
 --   Total size           : 180 = 5 * 36
 --   관리화면 유/무           : 유
---   P.K                  : BottleID
+--   P.K                  : EqpGroupID, EqpSeqNo
 --   관련 Table            : 
 --   이 력
 --          1. 2024-06-01 : 최초 생성
@@ -533,23 +565,24 @@ DROP TABLE tDailyEqpStatus;
 CREATE TABLE tDailyEqpStatus (
    EqpGroupID             CHAR(1) NOT NULL,         -- 설비군ID
    EqpSeqNo               CHAR(1) NOT NULL,         -- 호기 (1부터 시작)
-   EqpStatus              NVARCHAR(10),             -- 장비상태(Run, Idle, Trouble, Maintenance)
-   AccumulateTime         DATETIME                  -- 누적시간 (24시간 넘을수 없다)
+   SummaryDate            DATE NOT NULL,            -- 날짜
+   EqpStatus              NVARCHAR(10)  NOT NULL,   -- 장비상태(Run, Idle, Trouble, Maintenance)
+   AccumTmByOneDay        TIME                      -- 상태별 누적시간 (EndTime-StartTime, 24시간 넘을수 없다)
 ) ON [Process];
 
 ALTER TABLE tDailyEqpStatus
-       ADD  CONSTRAINT tDailyEqpStatus_PK PRIMARY KEY (EqpGroupID, EqpSeqNo) ON [ProcessIdx];
+       ADD  CONSTRAINT tDailyEqpStatus_PK PRIMARY KEY (EqpGroupID, EqpSeqNo, SummaryDate, EqpStatus) ON [ProcessIdx];
 ALTER TABLE tDailyEqpStatus 
       ADD CONSTRAINT tDailyEqpStatus_FK FOREIGN KEY (EqpGroupID, EqpSeqNo) REFERENCES tMstEqpGroup(EqpGroupID, EqpSeqNo) ON [ProcessIdx];
 ALTER TABLE tDailyEqpStatus
       ADD CONSTRAINT tDailyEqpStatus_CHK_status CHECK (EqpStatus IN ('Run', 'Idle', 'Trouble', 'Maintenance')) ON [ProcessIdx];
 ALTER TABLE tDailyEqpStatus
-      ADD CONSTRAINT tDailyEqpStatus_CHK_AccumulateTime CHECK (TIMEDIFF(AccumulateTime, '0000-01-01 00:00:00') <= '24:00:00') ON [ProcessIdx];
+      ADD CONSTRAINT tDailyEqpStatus_CHK_AccumTmByOneDay CHECK (TIMEDIFF(AccumTmByOneDay, '0000-01-01 00:00:00') <= '24:00:00') ON [ProcessIdx];
    
 -- ========================================================================================
 --   Table No             : 1
---   Table 명             : tProcBotHistOper
---   내  용                : LIMS Bottle 세정후에 tProcBotCurrOper에서 tProcBotHistOper 이동
+--   Table 명             : tHisProcBotOper
+--   내  용                : LIMS Bottle 세정후에 tProcBotOper에서 tHisProcBotOper 이동
 --
 --   성  격               : Process
 --   보존기간              : 영구
@@ -558,18 +591,17 @@ ALTER TABLE tDailyEqpStatus
 --   Record size          : 25
 --   Total size           : 180 = 5 * 36
 --   관리화면 유/무           : 유
---   P.K                  : BottleID
+--   P.K                  : BottleID, EqpGroupID, OperID, StartTime
 --   관련 Table            : 
 --   이 력
 --          1. 2024-06-01 : 최초 생성
 --
 -- ========================================================================================
-DROP TABLE tProcBotHistOper;
+DROP TABLE tHisProcBotOper;
 
-CREATE TABLE tProcBotHistOper (
-   BottleID               CHAR(1) NOT NULL,         -- 설비군ID
+CREATE TABLE tHisProcBotOper (
+   BottleID               CHAR(15) NOT NULL,        -- 설비군ID
    EqpGroupID             CHAR(1) NOT NULL,         -- 설비군 ID 
-   --OperGroupID          CHAR(1) NOT NULL,         -- Operation Group 
    OperID                 CHAR(1) NOT NULL,         -- 작업 ID  
    StartTime              DATETIME NOT NULL,        -- 착공시간
    EndTime                DATETIME,                 -- 완공시간
@@ -577,5 +609,36 @@ CREATE TABLE tProcBotHistOper (
    Position               CHAR(4)                   -- Bottle 반출입기, Stocker에서 위치정보
 ) ON [History];
 
-ALTER TABLE tProcBotHistOper
-      ADD  CONSTRAINT tProcBotHistOper_PK PRIMARY KEY (BottleID, EqpGroupID, OperID, StartTime) ON [HistoryIdx];
+ALTER TABLE tHisProcBotOper
+      ADD  CONSTRAINT tHisProcBotOper_PK PRIMARY KEY (BottleID, EqpGroupID, OperID, StartTime) ON [HistoryIdx];
+      
+-- ========================================================================================
+--   Table No             : 2
+--   Table 명             : tHisChgEqpStatus
+--   내  용                : LIMS Bottle 세정후에 tChgEqpStatus에서 tHisChgEqpStatus 이동
+--
+--   성  격               : Process
+--   보존기간              : 영구
+--   Record 발생건수(1일)   :
+--   Total Record 수      : 5
+--   Record size          : 25
+--   Total size           : 180 = 5 * 36
+--   관리화면 유/무           : 유
+--   P.K                  : EqpGroupID, EqpSeqNo, EqpStatus,StartTime
+--   관련 Table            : 
+--   이 력
+--          1. 2024-06-01 : 최초 생성
+--
+-- ========================================================================================
+DROP TABLE tHisChgEqpStatus;
+
+CREATE TABLE tHisChgEqpStatus (
+   EqpGroupID             CHAR(1) NOT NULL,         -- 설비군ID
+   EqpSeqNo               CHAR(1) NOT NULL,         -- 호기 (1부터 시작)
+   EqpStatus              NVARCHAR(10),             -- 장비상태(Run, Idle, Trouble, Maintenance)
+   StartTime              DATETIME,                 -- 시작시간
+   EndTime                DATETIME                  -- 종료시간
+) ON [History];
+
+ALTER TABLE tHisChgEqpStatus
+      ADD  CONSTRAINT tHisChgEqpStatus_PK PRIMARY KEY (EqpGroupID, EqpSeqNo, EqpStatus,StartTime) ON [HistoryIdx];    
